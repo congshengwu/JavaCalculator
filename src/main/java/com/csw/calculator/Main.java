5a93e0657c337462ec204123232a711bc5cabfd7
congshengwu <1024197824@qq.com>
2018-08-19 02:58:31 +0800

    2

--- src/main/java/com/csw/calculator/Main.java
+++ src/main/java/com/csw/calculator/Main.java
@@ -7,11 +7,13 @@ package com.csw.calculator;
 public class Main {
 
     public static void main(String[] args) {
+
         String result1 = Calculator.input("8×4-(10+12÷3)").getResult();
         System.out.println(result1);
 
         String result2 = Calculator.input("8/3").getResult(3); // 保留三位小数
         System.out.println(result2);
+
     }
 
 }
